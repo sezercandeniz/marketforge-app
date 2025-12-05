358f151a671b7225d0e168fe00e8a9955da17808
sezercandeniz <deniz.sezercan@gmail.com>
2025-12-05 21:40:23 +0000

    Fix after_install permissions

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+set -e
+
+echo ">>> Switching to project folder"
 cd /home/ubuntu/marketforge
-export PATH=$PATH:/usr/bin:/usr/local/bin
-npm install --omit=dev
+
+echo ">>> Fixing ownership"
+sudo chown -R ubuntu:ubuntu /home/ubuntu/marketforge
+
+echo ">>> Installing dependencies"
+npm install --unsafe-perm=true
+
+echo ">>> After install step completed"
