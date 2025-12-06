7827cf4c0e56a5d769943f8ed4a67b1291fbbf3e
sezercandeniz <deniz.sezercan@gmail.com>
2025-12-06 23:08:06 +0000

    Update API response message for CI/CD demo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send("MarketForge API running!");
+  res.status(200).send("MarketForge API UPDATED! CI/CD Pipeline Working ✔");
 });
 
 module.exports = app;
