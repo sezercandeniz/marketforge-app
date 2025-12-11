3623a6d70723702c3ee5abd76e27a81c12c9cd95
sezercandeniz <deniz.sezercan@gmail.com>
2025-12-11 19:56:04 +0000

    ca-class
    ca-class

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send("MarketForge API running 2");
+  res.status(200).send("MarketForge API running 1");
 });
 
 module.exports = app;
